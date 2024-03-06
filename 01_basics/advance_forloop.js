294d8faf1c7ed445e4bc32cb2fd41e22a33ffce7
Talib-Malik <133119471+Talib-Malik@users.noreply.github.com>
2024-03-06 18:06:55 +0000

    higherloop

--- 01_basics/advance_forloop.js
+++ 01_basics/advance_forloop.js
@@ -14,3 +14,59 @@ const greetings = "Hello world!"
 for (const greet of greetings) {
 console.log(`Each char is ${greet}`);    
 }
+//maps
+
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('USA', "united state of america")
+map.set('fr', "france")
+
+map.set('IN', "INDIA")  //we cannot use more map same as it only give one
+
+console.log(map);
+
+for (const [key, value] of object) {
+    console.log(key , ':-',value);
+}
+
+const myObject = {
+    'game1' : 'NFS',
+    'game2'  :'spiderman'
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key, ':-,value');
+    
+// }   doesnt work here
+//----------------------------------------------------------
+
+// four in loop
+
+const myObject2 ={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+
+for (const key in myObject2) {
+    console.log(`${key} shortcut is for ${myObject2[key]}`);
+
+           }
+
+
+           const programming= ["js","rb","py","java","cpp"]
+
+           for (const key in programming) {
+            // console.log(key); print only key numbers
+            // console.log(programming[key]); print the value
+           }
+
+
+
+        //for each loop    -----------------------------------------------------------
+
+
+
+        const coding = ["js","ruby","java","cpp","python"]
