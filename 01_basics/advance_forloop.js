ee9b6e02f3b833a4a53ea4cc0eec6cd7de1a5666
Talib-Malik <133119471+Talib-Malik@users.noreply.github.com>
2024-03-07 03:01:27 +0000

    advanceloop

--- 01_basics/advance_forloop.js
+++ 01_basics/advance_forloop.js
@@ -70,3 +70,42 @@ for (const key in myObject2) {
 
 
         const coding = ["js","ruby","java","cpp","python"]
+
+        // coding.forEach(function (val){
+        //     console.log(val);
+        // })
+
+
+// coding.forEach( (item) =>{
+// console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+ coding.forEach ( (item, index, arr)=>{
+console.log(item, index,arr);
+ })
+
+ const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName:"java"
+    }
+    {
+        languageName: "python",
+        languageFileName:"py"
+    }
+    {
+        languageName: "java",
+        languageFileName:"jav"
+    }
+ ]
+
+ myCoding.forEach ( (item)=>{
+    console.log(item.languageName);
+ })
+  
