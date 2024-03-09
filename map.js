970cf240b0f08c67ee584c5130c55dc28a22059f
Talib-Malik <133119471+Talib-Malik@users.noreply.github.com>
2024-03-09 05:01:45 +0000

    map and reduce

--- map.js
+++ map.js
@@ -1,9 +1,23 @@
-const myNumers = [1,2,3,4,5,6,7,8,9,10]
+// const myNumers = [1,2,3,4,5,6,7,8,9,10]
 
-// const newNums = myNumers.map( (num) => num + 10)   //if you apply scope then ypu have to write return
+// // const newNums = myNumers.map( (num) => num + 10)   //if you apply scope then ypu have to write return
 
-const newNums = myNumers
-            .map( (num) => num * 10)
-             .map( (num) => num + 1) 
-             .filter( (num) => num >= 40)
-console.log(newNums);
+// const newNums = myNumers
+//             .map( (num) => num * 10)
+//              .map( (num) => num + 1) 
+//              .filter( (num) => num >= 40)
+// console.log(newNums);
+
+
+
+
+
+
+
+
+// --------------------------reduce---------------
+
+
+const myNums = [1,2,3]
+
+ const myTotal = myNums.reduce(function(acc,curval))
